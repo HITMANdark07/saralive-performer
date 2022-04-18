e5ad8d36346254727092faae6c55cb59f5fa8784
HITMANdark07 <shekharkumar235838@gmail.com>
2022-04-19 02:04:45 +0530

    package name changed success

--- android/app/src/main/java/com/performer/MainActivity.java
+++ android/app/src/main/java/com/performer/MainActivity.java
@@ -1,4 +1,4 @@
-package com.poplyperformer;
+package com.poply.performer;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "poplyperformer";
+    return "performer";
   }
   @Override
   protected void onCreate(Bundle savedInstanceState) {
